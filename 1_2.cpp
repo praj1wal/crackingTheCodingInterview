368400d31dc7320a0b34de5f2cf0147da46d2a81
prajwal kotalwar <prajwalkotalwar@gmail.com>
2020-03-18 13:22:18 +0530

    added 1_2

---
+++ 1_2.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverseString(char *s1){
+    char *end=s1;
+    char temp;
+    if(s1){
+        while(*end){
+            end++;
+        }
+        end--;
+    }
+    while (s1<end){
+        temp=*s1;
+        *s1=*end;
+        *end=temp;
+        *s1++;
+        *end--;
+    }
+}
+
+int main(){
+    char s[20];
+    cout<<"Enter the string:";
+    gets(s);
+    cout<<s<<endl;
+    reverseString(s);
+    cout<<s;
+    return 0;
+}
